605ace0c4f976abfd4993fa388ab418ef74099fa
josuhernandez <ipartek2016>
2016-10-26 15:48:32 +0200

    Terminado PerroController y añadida clase FechaHora.java

--- perreraWS/src/main/java/com/ipartek/formacion/perrera/controller/PerroController.java
+++ perreraWS/src/main/java/com/ipartek/formacion/perrera/controller/PerroController.java
@@ -3,15 +3,20 @@ package com.ipartek.formacion.perrera.controller;
 
 import java.util.ArrayList;
 
+import javax.ws.rs.DELETE;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import com.ipartek.formacion.perrera.dao.PerroDAOImpl;
+import com.ipartek.formacion.perrera.pojo.FechaHora;
 import com.ipartek.formacion.perrera.pojo.Perro;
 
 import io.swagger.annotations.Api;
@@ -45,6 +50,119 @@ public class PerroController {
 			
 		} catch (Exception e) {
 			return Response.serverError().build();
+		}
+	}
+	
+	
+	
+	@GET
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@ApiOperation(value = "Busca un perro por su ID", notes = "devuelve un perro mediante el paso de su ID", response = Perro.class, responseContainer = "Perro")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Todo OK"),
+			@ApiResponse(code = 204, message = "No existe perro con esa ID"),
+			@ApiResponse(code = 500, message = "Error inexperado en el servidor") })
+	public Response getById(@PathParam("id") long idPerro) {
+
+		try {
+
+			PerroDAOImpl dao = PerroDAOImpl.getInstance();
+			Perro perro = (Perro) dao.getById(idPerro);
+
+			if (perro == null) {
+				return Response.noContent().build();
+			}
+			return Response.ok().entity(perro).build();
+		} catch (Exception e) {
+			return Response.serverError().build();
+		}
+	}
+	
+	@DELETE
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@ApiOperation(value = "Elimina un perro", notes = "Elimina un perro mediante el paso de su ID", response = Perro.class, responseContainer = "FechaHora")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Perro eliminado"),
+			@ApiResponse(code = 204, message = "No existe Perro con ese ID"),
+			@ApiResponse(code = 500, message = "Error inexperado en el servidor") })
+	public Response delete(@PathParam("id") int idPerro) {
+
+		try {
+			
+			PerroDAOImpl dao = PerroDAOImpl.getInstance();
+			Perro pElimnar = null;
+			
+			pElimnar = (Perro) dao.getById(idPerro);
+			
+			if (pElimnar == null) {
+				return Response.noContent().build();
+			} else {
+				dao.delete(pElimnar.getId());
+				return Response.ok().entity(new FechaHora()).build();
+			}
+		} catch (Exception e) {
+			return Response.serverError().build();
+		}
+	}
+	
+	
+	@POST
+	@Path("/{nombre}/{raza}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@ApiOperation(value = "Añade un perro", notes = "Crea y persiste un nuevo perro", response = Perro.class, responseContainer = "Perro")
+	@ApiResponses(value = { @ApiResponse(code = 201, message = "Perro Creado con exito"),
+			@ApiResponse(code = 409, message = "Perro ya Existente"),
+			@ApiResponse(code = 500, message = "Error inexperado en el servidor") })
+	public Response post(@PathParam("nombre") String nombrePerro, @PathParam("raza") String razaPerro) {
+		try {
+
+			PerroDAOImpl dao = PerroDAOImpl.getInstance();
+			Perro pCreado = new Perro(nombrePerro, razaPerro);
+			boolean creado = dao.insert(pCreado);
+						
+			
+			if (creado==true) {
+				return Response.status(201).entity(pCreado).build();
+			} else {
+				return Response.status(409).build();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return Response.serverError().build();
+		}
+	}
+	
+	
+	@PUT
+	@Path("/{id}/{nombre}/{raza}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@ApiOperation(value = "Modifica un perro", notes = "Modifica un perro ya existente mediante su identificador", response = Perro.class, responseContainer = "Perro")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Todo OK"),
+			@ApiResponse(code = 204, message = "No existe perro con ese ID"),
+			@ApiResponse(code = 409, message = "Perro existente, no se puede modificar"),
+			@ApiResponse(code = 500, message = "Error inexperado en el servidor") })
+	public Response put(@PathParam("id") int idPerro, @PathParam("nombre") String nombrePerro,
+			@PathParam("raza") String razaPerro) {
+		try {
+			
+			PerroDAOImpl dao = PerroDAOImpl.getInstance();
+			Perro pModificar = null;
+			
+			pModificar = (Perro) dao.getById(idPerro);
+			
+			if (pModificar == null) {
+				return Response.noContent().build();
+			} else {
+				pModificar.setNombre(nombrePerro);
+				pModificar.setRaza(razaPerro);
+				
+				dao.update(pModificar);
+				
+				return Response.ok().entity(pModificar).build();
+			}
+		} catch (Exception e) {
+			return Response.status(500).build();
+
 		}
 	}
 	
